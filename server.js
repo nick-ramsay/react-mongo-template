c39cb3f3c8c4d0bfd7a7a4f396c0edff3a273bef
nick-ramsay <NickRamsay1989@gmail.com>
2020-08-26 12:39:44 +1000

    Committing initial react app

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+
+const mongoose = require("mongoose");
+//const routes = require("./routes");
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serve up static assets (usually on hernpoku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+//Setting headers for CORS Policies
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Credentials");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
+
+
+// Add routes, both API and view
+app.use(routes);
+
+// Connect to the Mongo DB
+//mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/application-portal", {useNewUrlParser: true, useUnifiedTopology: true});
+
+// Start the API server
+app.listen(PORT, function() {
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+});

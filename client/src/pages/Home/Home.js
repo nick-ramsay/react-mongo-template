bb89c143564ff4534679a4435e494c5bc9b1f3c8
Nick Ramsay <NickRamsay1989@gmail.com>
2020-08-26 20:36:16 +1000

    Updated prepped layout

---
+++ client/src/pages/Home/Home.js
@@ -0,0 +1,48 @@
+import React, { Component, useState, useEffect } from 'react';
+import logo from '../../../src/logo.svg';
+import "./style.css";
+
+const override = "display: block; margin: 0 auto; border-color: indigo;";
+
+const Home = () => {
+
+    return (
+        <div>
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <p>Edit <code>src/App.js</code> and save to reload.</p>
+                    <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">Learn React</a>
+                </header>
+                <form className="form-inline">
+                    <div className="row mt-3">
+                        <div className="col">
+                            <label for="staticEmail2" className="sr-only">Email</label>
+                            <input type="text" placeholder="Enter Test Message Here" />
+                        </div>
+                        <div className="col">
+                            <button type="button" className="btn btn-sm btn-custom">Save Message</button>
+                        </div>
+                    </div>
+                </form>
+                <div className="col-md-12 mt-5">
+                    <table class="table table-dark">
+                        <thead>
+                            <tr>
+                                <th scope="col">Messages</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>No Messages</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div >
+    )
+
+}
+
+export default Home;

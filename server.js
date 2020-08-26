168628d67dcbf3b47c3291799b9a542a1ac2f90e
Nick Ramsay <NickRamsay1989@gmail.com>
2020-08-26 19:21:51 +1000

    Cleaned up template, removed unneccessary packages

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require("express");
 
-//const mongoose = require("mongoose");
 //const routes = require("./routes");
 
 const PORT = process.env.PORT || 3001;
@@ -9,7 +8,8 @@ const app = express();
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// Serve up static assets (usually on hernpoku)
+
+// Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
@@ -26,9 +26,6 @@ app.use(function(req, res, next) {
 
 // Add routes, both API and view 
 //app.use(routes);
-
-// Connect to the Mongo DB
-//mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/application-portal", {useNewUrlParser: true, useUnifiedTopology: true});
 
 // Start the API server
 app.listen(PORT, function() {

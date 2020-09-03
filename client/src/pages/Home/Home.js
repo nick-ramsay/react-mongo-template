666abdab0bb68aa3cdcd5e0a2bef5a44dbba5318
Nick Ramsay <NickRamsay1989@gmail.com>
2020-09-03 18:14:35 +1000

    Final react barebones changes made, can now be used for cloning

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -14,31 +14,6 @@ const Home = () => {
                     <p>Edit <code>src/App.js</code> and save to reload.</p>
                     <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">Learn React</a>
                 </header>
-                <form className="form-inline">
-                    <div className="row mt-3">
-                        <div className="col">
-                            <label for="staticEmail2" className="sr-only">Email</label>
-                            <input type="text" placeholder="Enter Test Message Here" />
-                        </div>
-                        <div className="col">
-                            <button type="button" className="btn btn-sm btn-custom">Save Message</button>
-                        </div>
-                    </div>
-                </form>
-                <div className="col-md-12 mt-5">
-                    <table class="table table-dark">
-                        <thead>
-                            <tr>
-                                <th scope="col">Messages</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>No Messages</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
             </div>
         </div >
     )

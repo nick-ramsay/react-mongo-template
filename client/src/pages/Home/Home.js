ab234ebd9f1165cca34ce01dd712ea0fc2b1c549
nick-ramsay <NickRamsay1989@gmail.com>
2020-09-10 13:53:21 +1000

    input and table now set up for test messages

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -13,6 +13,30 @@ const Home = () => {
                     <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">Learn React</a>
                 </header>
             </div>
+            <form className="mt-3">
+                <div className="form-row text-center">
+                    <div className="col">
+                        <input type="text" className="form-control" placeholder="Enter a new test message" />
+                    </div>
+                </div>
+                <div className="form-row text-center">
+                    <div className="col mt-3">
+                        <div type="button" className="btn btn-sm btn-custom">Submit</div>
+                    </div>
+                </div>
+            </form>
+            <table className="table table-striped table-dark mt-3">
+                <tbody>
+                    <tr className="text-center">
+                        <td>Test Message 1</td>
+                        <td><div type="button" className="btn btn-sm btn-custom-red">Delete</div></td>
+                    </tr>
+                    <tr className="text-center">
+                        <td>Test Message 2</td>
+                        <td><div className="btn btn-sm btn-custom-red">Delete</div></td>
+                    </tr>
+                </tbody>
+            </table>
         </div >
     )
 

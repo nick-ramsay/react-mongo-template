45f6c2dd01e3a6c5610149bb974a047786322910
nick-ramsay <NickRamsay1989@gmail.com>
2020-09-24 16:34:37 +1000

    Synced layouts between mongodb and mysql template apps

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -46,7 +46,7 @@ const Home = () => {
         <div>
             <div className="App">
                 <header className="App-header">
-                    <h1>React Mongo Template</h1>
+                    <h1>React MongoDB Template</h1>
                     <img src={logo} className="App-logo" alt="logo" />
                     <p>Edit <code>src/App.js</code> and save to reload.</p>
                     <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">Learn React</a>

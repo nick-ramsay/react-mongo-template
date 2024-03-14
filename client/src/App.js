57d5741cf09e12cf013dc1df11f5703b39ab0a96
Nick Ramsay <nickramsay@nlb-int-svc-proxy-06a01ef5ff9ffd47.elb.us-gov-west-1.amazonaws.com>
2024-03-14 14:25:07 +1100

    Testing out allowedTracingURLs for RUMS-2475

--- client/src/App.js
+++ client/src/App.js
@@ -8,32 +8,44 @@ import Home from "../src/pages/Home/Home";
 import { datadogRum } from '@datadog/browser-rum';
 
 datadogRum.init({
-    applicationId: keys.ddrum.react_rum_application_id,
-    clientToken: keys.ddrum.react_rum_client_token,
-    site: 'datadoghq.com',
-    service:'react-mongo-template',
-    env:'staging', 
-    version: '1.0.24',
-    sessionSampleRate: 100,
-    sessionReplaySampleRate: 100,
-    traceSampleRate: 10,
-    trackUserInteractions: true,
-    trackResources: true,
-    trackLongTasks: true,
-    defaultPrivacyLevel:'mask-user-input',
-    allowedTracingUrls: ["http://localhost:3001/", "https://react-mongo-template.herokuapp.com/"]
+  applicationId: keys.ddrum.react_rum_application_id,
+  clientToken: keys.ddrum.react_rum_client_token,
+  site: 'datadoghq.com',
+  service: 'react-mongo-template',
+  env: 'staging',
+  version: '1.0.24',
+  sessionSampleRate: 100,
+  sessionReplaySampleRate: 100,
+  traceSampleRate: 10,
+  trackUserInteractions: true,
+  trackResources: true,
+  trackLongTasks: true,
+  defaultPrivacyLevel: 'mask-user-input',
+  //allowedTracingUrls: ["http://localhost:3001/", "https://react-mongo-template.herokuapp.com/"],
+  /*allowedTracingUrls: [
+    { match: [/https:\/\/qa\.ascap\.com/, /https:\/\/ma-api-qa\.ascap\.com/], propagatorTypes: ["datadog"]}
+  ]*/
+  allowedTracingUrls: [
+    { match: [/http:\/\/localhost\:3001/, /https\:\/\/react-mongo-template\.herokuapp\.com/], propagatorTypes: ["traceparent"] }
+  ]
+/*
+allowedTracingUrls: [
+  { match: /http:\/\/localhost\:3001/, propagatorTypes: ["traceparent"]},
+  { match: /https\:\/\/react-mongo-template\.herokuapp\.com/, propagatorTypes: ["traceparent"]}
+]
+*/
 });
-    
+  
 datadogRum.startSessionReplayRecording();
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-          <Route exact path="/" element={<Home />} />
-      </Routes>
-    </Router>
-  );
+return (
+  <Router>
+    <Routes>
+        <Route exact path="/" element={<Home />} />
+    </Routes>
+  </Router>
+);
 }
 
 export default App;

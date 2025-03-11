970186dacca32e566961efa680af424bf5d66aa5
Nick Ramsay <NickRamsay1989@gmail.com>
2025-03-11 18:48:31 +1100

    Allowing all cors

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ app.use(function (req, res, next) {
   const origin = req.headers.origin;
 
   if (allowedOrigins.includes(origin)) {
-    res.header("Access-Control-Allow-Origin", origin);
+    res.header("Access-Control-Allow-Origin", "*"); //origin);
   }
 
 
